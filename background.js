f2f53c45239cbd43cc1c109f572ee741fa258d47
Eduard1997 <ghergheluca_edi@yahoo.com>
2018-10-31 23:20:02 +0200

    blind page added

--- background.js
+++ background.js
@@ -17,51 +17,24 @@ chrome.runtime.onInstalled.addListener(function() {
           actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
-
 });
-/*let winID;
-chrome.contextMenus.onClicked.addListener(function proccess_interested(info, tab){
-
-    chrome.tabs.create({active: false}, function(newTab) {
-
-        // After the tab has been created, open a window to inject the tab into it.
-        chrome.windows.create(
-            {
-                tabId:      newTab.id,
-                type:       "popup",
-                url:        chrome.extension.getURL('color_blind.html'),
-                focused: true
-            },function(window){
-                winID = newWindow.id;
-            });
+window.addEventListener('load', function() {
+    let blind_button = document.getElementById('spoderblind');
+    blind_button.addEventListener('click', function() {
+        document.getElementById('front-section').style.display = 'none';
+        document.getElementById('blind-section').style.display = 'block';
     });
-});*/
-document.addEventListener('DOMContentLoaded', function() {
-  let link = document.getElementById('spoderblind');
-  link.addEventListener('click', function() {
-      chrome.tabs.query(
-          {currentWindow: true, active : true},
-          function(tabArray){
-              chrome.windows.remove(tabArray[0].id);
-          }
-      );
 
-      let winID;
-      chrome.tabs.create({active: false}, function(newTab) {
-
-          // After the tab has been created, open a window to inject the tab into it.
-          chrome.windows.create(
-              {
-                  tabId:      newTab.id,
-                  type:       "popup",
-                  url:        chrome.extension.getURL('color_blind.html'),
-                  focused: true
-              },function(window){
-                  winID = newWindow.id;
-              });
-      });
-  });
+    let home_button = document.getElementById('home-page-btn');
+    home_button.addEventListener('click', function() {
+        document.getElementById('front-section').style.display = 'block';
+        document.getElementById('blind-section').style.display = 'none';
+    });
 });
+
+
+
+
 
 
 
