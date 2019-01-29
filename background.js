b69a0da23a16138144090bf9778e11d77ca8ac49
Eduard1997 <ghergheluca_edi@yahoo.com>
2019-01-29 21:59:49 +0200

    color blind js functionality. user input added

--- background.js
+++ background.js
@@ -32,17 +32,61 @@ window.addEventListener('load', function() {
     });
     let blind_changes = document.getElementById("apply-blind-changes");
     blind_changes.addEventListener('click', saveBlindChanges);
+    let other_input = document.getElementById("other");
+    other_input.addEventListener('change', addUserInput);
+    let revert_chnages = document.getElementById("revert-changes");
+    revert_chnages.addEventListener('click', revertChanges);
 });
 
 function saveBlindChanges() {
-    let x = document.getElementById("check1").value;
+    let bg_color = document.getElementById("bg_color");
+    let paragraph = document.getElementById("paragraph");
+    let links = document.getElementById("links");
+    let font_color = document.getElementById("font_color");
+    let other_input = document.getElementById("other");
+    let checkedIssues = {};
+    let color = document.getElementById("color-picker").value;
+
+    if(bg_color.checked == true) {
+        checkedIssues['bg_color'] = true;
+    }
+    if(paragraph.checked == true) {
+        checkedIssues['paragraph'] = true;
+    }
+    if(links.checked == true) {
+        checkedIssues['links'] = true;
+    }
+    if(font_color.checked == true) {
+        checkedIssues['font_color'] = true;
+    }
+    if(other_input.checked == true) {
+        let other_section = document.getElementById("other_section").value;
+        if(other_section) {
+            checkedIssues["section_new"] = other_section;
+        }
+    }
+    checkedIssues["color"] = color;
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
+        chrome.tabs.sendMessage(activeTab.id, {"blind_changes": checkedIssues});
     });
 }
 
+function addUserInput() {
+    let other_input = document.getElementById("other");
+    if(other_input.checked == true) {
+        document.getElementById("other_section").style.display = 'block';
+    } else {
+        document.getElementById("other_section").style.display = 'none';
+    }
+}
 
+function revertChanges() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"revert_changes": true});
+    });
+}
 
 
 
--- content.js
+++ content.js
@@ -1,6 +1,79 @@
-console.log("am intrat in content js");
+var new_section;
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(request.message)
+        if(request.blind_changes) {
+          if(typeof request.blind_changes.links !== 'undefined') {
+              changeLinkColor(request.blind_changes.color);
+          }
+          if(typeof request.blind_changes.bg_color !== 'undefined') {
+              changeBackgroundColor(request.blind_changes.color);
+          }
+          if(typeof request.blind_changes.paragraph !== 'undefined') {
+              changeParagraphColor(request.blind_changes.color);
+          }
+          if(typeof request.blind_changes.font_color !== 'undefined') {
+                changeFontColor(request.blind_changes.color);
+          }
+          if(typeof request.blind_changes.section_new !== 'undefined') {
+              let sectionNew = document.getElementsByTagName(request.blind_changes.section_new);
+              if(sectionNew.length == 0) {
+                  new_section = '';
+                  alert("The element ---> " + request.blind_changes.section_new + " <-- is not on this page or is not valid");
+              } else {
+                  new_section = request.blind_changes.section_new;
+                  changeSectionNewColor(request.blind_changes.color, request.blind_changes.section_new);
+              }
+          }
+        }
+        if(request.revert_changes) {
+            revertModifications();
+        }
     }
 );
+
+function changeLinkColor(color) {
+    let link = document.getElementsByTagName("a");
+    for(let i = 0; i <  link.length; i++) {
+        link[i].style.color = color;
+    }
+}
+function changeBackgroundColor(color) {
+    document.getElementsByTagName("body")[0].style.backgroundColor = color;
+}
+function changeParagraphColor (color) {
+    let paragraphs = document.getElementsByTagName("p");
+    for(let i = 0; i <  link.length; i++) {
+        paragraphs[i].style.color = color;
+    }
+}
+function changeFontColor(color) {
+    document.getElementsByTagName("body")[0].style.color = color;
+}
+function changeSectionNewColor(color, section) {
+    let section_new = document.getElementsByTagName(section);
+    for(let i = 0; i < section_new.length; i++) {
+        section_new[i].style.color = color;
+    }
+}
+function revertModifications(){
+    let link = document.getElementsByTagName("a");
+    if(typeof link.length != 0) {
+        for(let i = 0; i <  link.length; i++) {
+            link[i].style.color = 'initial';
+        }
+    }
+    let paragraphs = document.getElementsByTagName("p");
+    if(paragraphs.length != 0) {
+        for(let i = 0; i <  link.length; i++) {
+            paragraphs[i].style.color = 'initial';
+        }
+    }
+    if(new_section) {
+        let section_new = document.getElementsByTagName(new_section);
+        for(let i = 0; i < section_new.length; i++) {
+            section_new[i].style.color = 'initial';
+        }
+    }
+    document.getElementsByTagName("body")[0].style.backgroundColor = 'initial';
+    document.getElementsByTagName("body")[0].style.color = 'initial';
+}
